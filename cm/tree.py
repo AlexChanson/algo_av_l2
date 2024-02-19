5f5c182fcea40546580c421c6b982b329fbdad39
Alexandre Chanson <a.chanson.tours@gmail.com>
2024-02-19 12:52:21 +0100

    added binary tree

--- cm/tree.py
+++ cm/tree.py
@@ -1,3 +1,12 @@
 class AB:
- def __init__(self):
-     pass
+    def __init__(self, etiquette, gauche=None, droite=None):
+        self.etiquette = etiquette
+        self.g = gauche
+        self.d = droite
+
+    def estFeuille(self):
+        return self.d is None and self.g is None
+
+
+def estVide(A):
+    return A is None
